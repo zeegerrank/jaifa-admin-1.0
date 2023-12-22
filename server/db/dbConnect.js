f033eba30c6c14f977c5bd8c8b7c390bb782446d
zeegerrank <zeegerrank@gmail.com>
2023-12-22 11:38:34 +0700

    config db and server

--- server/db/dbConnect.js
+++ server/db/dbConnect.js
@@ -1,10 +1,11 @@
 const db = require("mongoose");
 const DB_URI = process.env.DB_URI;
-const dbConnect = () => {
+const dbConnect = (cb) => {
   db.connect(DB_URI);
 
   db.connection.once("open", () => {
     console.log("Database is connected!");
+    cb();
   });
 };
 
--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
-require("dotenv").config();
-const express = require("express");
+require("dotenv").config();const express = require("express");
 const app = express();
 
 app.use(express.json());
@@ -8,10 +7,14 @@ app.post("/", (req, res) => {
   return res.status(200).send({ message: "Hello world" });
 });
 
+//**set up server */
 const PORT = process.env.PORT || 3500;
-app.listen(PORT, () => {
-  console.log("App is running on port", PORT);
-});
+const openServer = () => {
+  app.listen(PORT, () => {
+    console.log("App is running on port", PORT);
+  });
+};
 
+//** connect to db => open server */
 const dbConnect = require("./db/dbConnect");
-dbConnect();
+dbConnect(openServer);
