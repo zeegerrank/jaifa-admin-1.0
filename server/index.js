652e4d302c5e718e04fa10dd32eb882cf00f669a
zeegerrank <zeegerrank@gmail.com>
2023-12-22 10:51:56 +0700

    server init

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  return res.status(200).send({ message: "Hello world" });
+});
+
+const PORT = process.env.PORT || 3500;
+app.listen(PORT, () => {
+  console.log("App is running on port", PORT);
+});
